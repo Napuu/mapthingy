dc5064120c27955dd59dab0592e12723ba0c54c2
Kääriäinen Santeri <kaarias3@fractran.org.aalto.fi>
2020-03-01 11:20:40 +0200

    now joining ready mbtiles

--- geojsons/main.js
+++ geojsons/main.js
@@ -2,18 +2,24 @@ const tippecanoe = require('tippecanoe');
 const fs = require('fs');
 const walk = require('walk');
 const configurations = require('./configurations.js');
-//console.log(test);
+const _exec = require('child_process').exec;
+const util = require('util');
+const exec = util.promisify(_exec);
+const _ = require('underscore');
 
 const walker = walk.walk("tiles", {});
 
-walker.on("file", (root, stats, next)  => {
+walker.on("file", (root, stats, next)  => { // walker is not actually useful at all in this case :-) fs.readdir would be enough
   if (stats.size < 1000000) {
     console.log(`size of "${stats.name}" under 1mb, maybe something is missing?`);
   }
+
   const noExtension = stats.name.split(".")[0];
   const tile = noExtension.split("_")[0];
   const layer = noExtension.split("_")[1];
+
   function handleTileTippecanoe(srcName) {
+    return; // remove this
     const path = `./${root}/${stats.name}`;
     if (configurations[srcName] === undefined) {
       console.log(`skipping ${srcName}, no matching configuration found`);
@@ -24,7 +30,29 @@ walker.on("file", (root, stats, next)  => {
     }
   }
   handleTileTippecanoe(layer);
+
   next();
 });
 
+async function handleTileJoin(groupedByLayer) {
+  const keys = Object.keys(groupedByLayer);
+  for (let i = 0; i < keys.length; i++) {
+    const layerName = keys[i];
+    const files = groupedByLayer[layerName];
+    const cmd = `tile-join -pk -f -o ${layerName} ${files.join(' ')}`;
+    console.log(cmd);
+    //await exec(cmd);
+  }
+}
+
+walker.on("end", () => {
+  console.log("tippecanoe done, now joining tiles");
+  const files = fs.readdirSync(".");
+  const mbtilesFiles = files.filter(fileName => fileName.endsWith("mbtiles") && fileName.indexOf("_") !== -1);
+  const groupedByLayer = _.groupBy(mbtilesFiles, (file) => {
+    return file.split("_")[1];
+  });
+  console.log(groupedByLayer);
+  handleTileJoin(groupedByLayer);
+});
 
